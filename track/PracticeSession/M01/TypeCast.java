7e5cfa4648c05541190e855e2045f3c931648969
Aklanta <aklantaswain@gmail.com>
2026-08-03 21:07:39 +0530

    Solved

---
+++ track/PracticeSession/M01/TypeCast.java
@@ -0,0 +1,18 @@
+public class Main {
+private static boolean dailyTarget;
+public static void main(String[] args) {
+int solved Problems = 47;
+int practiceDays = 5;
+double exactPercentage = 86.75;
+double reportValue = solved Problems;
+int wholePer = (int) exactPercentage;
+double averagePerDay reportValue / practiceDays;
+if (averagePerDay >= 9.0) {
+dailyTarget = true;
+}
+System.out.println("Report Value: + reportValue);
+System.out.println("Whole Percentage: " + wholePer);
+System.out.println("Average Per Day: + averagePerDay);
+System.out.println("Daily Target Reached: + dailyTarget);
+}
+}
