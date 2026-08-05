0d808bfb3aacce89aef397afb86aaf10116c49dc
Aklanta <aklantaswain@gmail.com>
2026-08-05 11:16:47 +0530

    feat: add Task5 to implement day-of-week learning/revision schedule using switch statement

--- track/M02/Task5.java
+++ track/M02/Task5.java
@@ -7,12 +7,12 @@ case 2:
 case 3:
 case 4:
 case 5:
-System.out.println("Learning Day");
-break;
+           System.out.println("Learning Day");
+           break;
 case 6:
 case 7:
-System.out.println("Revision Day");
-break;
+          System.out.println("Revision Day");
+          break;
 default:
 System.out.println("INVALID day");
 }
