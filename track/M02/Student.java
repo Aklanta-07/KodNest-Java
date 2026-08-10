4d49667c3b5281debbb8bea886403988903a9331
Aklanta <aklantaswain@gmail.com>
2026-08-10 23:04:53 +0530

    feat: add Student class and implementation for reading and displaying student profile data

---
+++ track/M02/Student.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Student {
+    int id;
+    String name;
+    String course;
+    double javaScore;
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Create one Student object
+        Student student = new Student();
+        
+        // Read and store all values in the object
+        student.id = scanner.nextInt();
+        student.name = scanner.next();
+        student.course = scanner.next();
+        student.javaScore = scanner.nextDouble();
+        
+        // Display the values stored in the object
+        System.out.println("Student Profile");
+        System.out.println("ID: " + student.id);
+        System.out.println("Name: " + student.name);
+        System.out.println("Course: " + student.course);
+        System.out.println("Java Score: " + student.javaScore);
+    }
+}
