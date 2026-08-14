80a05cac8ef90563c38a8873ee980b68d1c24fd2
Aklanta <aklantaswain@gmail.com>
2026-08-14 11:09:34 +0530

    feat: add StudentClient and StudentDemo classes for student data input and display

---
+++ track/M02/constructor/StudentClient.java
@@ -0,0 +1,9 @@
+package constructor;
+
+public class StudentClient {
+    public static void main(String[] args) {
+            StudentDemo stud = new StudentDemo();
+            stud.input("Raja", 20);
+            stud.display();
+    }
+}
