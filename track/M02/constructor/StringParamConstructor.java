5e6f53d97cd77355898684e999378c5027bc5a3d
Aklanta <aklantaswain@gmail.com>
2026-08-14 12:49:32 +0530

    feat: add StringParamConstructor class for book input and display

---
+++ track/M02/constructor/StringParamConstructor.java
@@ -0,0 +1,30 @@
+package constructor;
+
+import java.util.Scanner;
+
+class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
+ class MainThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+
+        Book book = new Book(title, author);
+        book.display();
+    }
+}
