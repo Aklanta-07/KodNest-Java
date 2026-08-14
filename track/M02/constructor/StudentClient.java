427896c7730fa2fb5e6667f7e33c22f48ed46e3c
Aklanta <aklantaswain@gmail.com>
2026-08-14 11:10:40 +0530

    feat: extend StudentDemo input method to include height and update display

--- track/M02/constructor/StudentClient.java
+++ track/M02/constructor/StudentClient.java
@@ -3,7 +3,7 @@ package constructor;
 public class StudentClient {
     public static void main(String[] args) {
             StudentDemo stud = new StudentDemo();
-            stud.input("Raja", 20);
+            stud.input("Raja", 20, 170);
             stud.display();
     }
 }
--- track/M02/constructor/StudentDemo.java
+++ track/M02/constructor/StudentDemo.java
@@ -3,15 +3,18 @@ package constructor;
 public class StudentDemo {
     String name;
     int age;
+    int height;
 
-    public void input(String name, int age) {
+    public void input(String name, int age, int height) {
         this.name = name;
         this.age = age;
+        this.height = height;
     }
 
     public void display() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
     }
 
 }
